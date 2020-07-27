84611c19ad9bd4d4f66321d76bc0bea31f26e63a
Toby Sykes <toby.sykes@unknown.home>
2020-07-27 17:05:37 +0100

    favicon and filepath fix

--- toby-cv-site/website-code/inc/nav.php
+++ toby-cv-site/website-code/inc/nav.php
@@ -1,7 +1,7 @@
 
 <div class="container">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img src="./img/toby.png" height="100" alt="Site logo stylised flower with letter T"></a>
+    <a class="navbar-brand" href="/"><img src="/img/toby.png" height="100" alt="Site logo stylised flower with letter T"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
